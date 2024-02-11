b984c4acb7014fa05058ed42332c487c253dd216
rakeshls <73830663+rakeshls@users.noreply.github.com>
2024-02-11 19:55:59 +0530

    Add files via upload

---
+++ controllers/newRobot_controller/newRobot_controller.py
@@ -0,0 +1,38 @@
+from controller import Robot, Keyboard, Motion
+
+timestep=32
+
+robot= Robot()
+keyboard = Keyboard()
+keyboard.enable(timestep)
+
+forward = Motion('../../motions/Forwards50.motion')
+backward = Motion('../../motions/Backwards.motion')
+sideStepLeft = Motion('../../motions/SideStepLeft.motion')
+sideStepRight = Motion('../../motions/SideStepRight.motion')
+
+def startMotion(motion):
+    motion.play()
+   
+def printMessages():
+    print('press up to move forward')
+    print('press down to move backward')
+    print('press left for side step left')
+    print('press right for side step right')
+     
+key = -1
+
+printMessages()
+
+while robot.step(timestep) != -1:
+
+    key = keyboard.getKey()  
+
+    if key == Keyboard.LEFT:
+        startMotion(sideStepLeft)
+    elif key == Keyboard.RIGHT:
+        startMotion(sideStepRight)
+    elif key == Keyboard.UP:
+        startMotion(forward)
+    elif key == Keyboard.DOWN:
+        startMotion(backward)
